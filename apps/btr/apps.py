41ffd19409246b4f0a780a94dabfd7216659a46c
Mikhail <mpetrov@almasc.ru>
2025-11-25 22:26:54 +0300

    chore(btr): cleanup unused files and update config
    - Remove unused admin.py, apps.py, views.py from btr app
    - Remove deprecated default_app_config from __init__.py (Django 3.2+)
    - Fix .gitignore BTR path pattern

--- apps/btr/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class BtrConfig(AppConfig):
-    """Конфигурация приложения BTR (BeachTennisRussia)."""
-    
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.btr'
-    verbose_name = 'BTR Рейтинги'
